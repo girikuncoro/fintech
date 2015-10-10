c9be7a2a084bd7c65adbd0b0049118c6f26cc608
girikuncoro <girikuncoro@gmail.com>
2015-10-10 15:16:38 -0400

    Restructured flask templating and static

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
-PORT = 5000
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
-    return "Hello Fintech!"
+	return redirect(url_for('login'))
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/auth")
+def auth():
+	return True
 
 if __name__ == "__main__":
-    app.run(port=PORT, debug=True)
+    app.run(debug=True)
