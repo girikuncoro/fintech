f3e788c35a96d1bb1123142a27b4c4617c6f4a69
Ubuntu64 <divyeshdarde@github.com>
2015-10-10 21:12:02 -0400

    Added transaction backend

---
+++ transaction/transaction.py
@@ -0,0 +1,39 @@
+import psycopg2
+import os
+import simplejson as json
+
+# Dummy for local deployment
+# Remove when deploying to Heroku
+
+TRANSACTION_TABLE="Transaction"
+dbname=os.environ['DBNAME']
+dbuser=os.environ['DBUSER']
+dbpass=os.environ['DBPASS']
+dbhost=os.environ['DBHOST']
+
+conn = psycopg2.connect("dbname='"+dbname+"' user='"+dbuser+"' host='"+dbhost+"' password='"+dbpass+"'")
+cur=conn.cursor()
+
+def get_transactions_for_id(userName):
+    
+    cur.execute("SELECT * FROM Transaction WHERE Transaction.fromUser="+userName+" OR Transaction.toUser="+toUser)
+    
+    #transactionId, fromUser, toUser, amount, transDate
+    data = {}
+    lst=[]
+    
+    for row in cur.fetchall():
+        data['transactionId'] = row[0]
+        data['fromUser'] = row[1]
+        data['toUser'] = row[2]
+        data['amount'] = row[3]
+        data['transDate'] = row[4]
+        lst.append(data)
+
+    return json.dumps(lst)
+
+def main():
+    print(get_transactions_for_id("divyesh"))
+
+if __name__ == "__main__":
+    main()
