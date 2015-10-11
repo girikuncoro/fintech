5379ccd12db9bb146e1f5d373f018ba793deb3f4
Allie Meng <awm79@cornell.edu>
2015-10-10 21:33:52 -0400

    sns code

---
+++ Modern/ModernTests/ModernTests.m
@@ -0,0 +1,39 @@
+//
+//  ModernTests.m
+//  ModernTests
+//
+//  Created by Allie Meng on 10/10/15.
+//  Copyright © 2015 Allie Meng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ModernTests : XCTestCase
+
+@end
+
+@implementation ModernTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
